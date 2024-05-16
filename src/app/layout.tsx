36923c7a3864f5b7a7e7365e5f1b8f83699b7681
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-05-16 23:41:49 +0300

    clean: remove unnecessay comments in the app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
-// "use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers";
-import { Notification } from "@/shared/notification";
-// import { useNotificationStore } from "@/store/notification";
-import { useAppDispatch, useAppSelector } from "@/hooks/redux";
-import { hideCardNotification } from "@/store/actions/notification";
 import NotificationInitializer from "@/shared/notificationInitializer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,30 +16,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // // const notification = useNotificationStore((state) => state.notification);
-  // const dispatch = useAppDispatch();
-  // const hideNotificationHandler = () => {
-  //   // const { hideCardNotification } = useNotificationStore();
-  //   dispatch(hideCardNotification());
-  // };
-
-  // const notification = useAppSelector((state) => state.notification);
-
   return (
     <Providers>
       {/* // <html lang="en" data-theme="dark">
     // TODO: add dynamic changing of themes */}
       <html lang="en">
         <body className={inter.className}>
-          {/* {notification.showCardNotification && (
-            <div>
-              <Notification
-                type={notification.cardNotificationType}
-                message={notification.cardMessage}
-                onClose={hideNotificationHandler}
-              />
-            </div>
-          )} */}
           <NotificationInitializer />
           {children}
         </body>
