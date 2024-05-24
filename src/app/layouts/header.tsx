3d37a853562a9b83e15a7e9c014249fd5f0cace1
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-05-24 17:15:36 +0300

    feat: provide theme icon and current user data in the header

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import Image from "next/image";
+import { ThemeController } from "@/app/shared/themeController";
+import { CurrentUser } from "@/app/shared/currentUser";
 
 export const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <header
+      className="w-full flex items-center justify-between 
+      p-4 bg-color-3 border-b-[1px] border-color-3"
+    >
+      <div className="flex items-center gap-2">
+        <Image src="/logo.png" width={40} height={40} alt="logo" />
+        <span className="text-color-1 font-semibold">AppCrons</span>
+      </div>
+      <div className="flex items-center gap-4">
+        {/* <div>Theme Controller</div> */}
+        <ThemeController />
+        <CurrentUser />
+      </div>
+    </header>
+  );
 };
