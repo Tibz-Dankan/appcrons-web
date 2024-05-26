68ca26081a152ae7df8e39454fa419e892dab2b9
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-05-26 11:20:24 +0300

    feat: integrate shadcn ui lib

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers";
 import NotificationInitializer from "@/app/shared/notificationInitializer";
 import { AppLayout } from "@/app/layouts/appLayout";
 import { ThemeAppProvider } from "@/providers/themeProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Keep Active",
@@ -21,7 +30,12 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en" suppressHydrationWarning>
-        <body className={inter.className}>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
           <ThemeAppProvider>
             <NotificationInitializer />
             <AppLayout>{children}</AppLayout>
