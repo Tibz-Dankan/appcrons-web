f48dae1440e7352dd7af35f51f992cf905e176ef
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-05-26 11:21:11 +0300

    Revert "feat: integrate shadcn ui lib"
    This reverts commit 68ca26081a152ae7df8e39454fa419e892dab2b9.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers";
 import NotificationInitializer from "@/app/shared/notificationInitializer";
 import { AppLayout } from "@/app/layouts/appLayout";
 import { ThemeAppProvider } from "@/providers/themeProvider";
 
-// const inter = Inter({ subsets: ["latin"] });
-
-import { Inter as FontSans } from "next/font/google";
-
-import { cn } from "@/lib/utils";
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Keep Active",
@@ -30,12 +21,7 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
+        <body className={inter.className}>
           <ThemeAppProvider>
             <NotificationInitializer />
             <AppLayout>{children}</AppLayout>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,84 +1,34 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
-const config = {
-  darkMode: ["class"],
+const config: Config = {
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-      },
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        primary: "#087f5b", //Teal-9
+        secondary: "#1098ad", //Cyan-7
+        "color-text-primary": "var(--color-text-primary)",
+        "color-text-secondary": "var(--color-text-secondary)",
+        "color-border-primary": "var(--color-border-primary)",
+        "color-bg-primary": "var(--color-bg-primary)",
+        "color-bg-secondary": "var(--color-bg-secondary)",
+        "color-bg-tertiary": "var(--color-bg-tertiary)",
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        slideDown: "slideDown 0.5s ease-out forwards",
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { opacity: "0", transform: "translateY(-150px)" },
+          "100%": { opacity: "1", transform: "translateY(0px)" },
+        },
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config;
-
+  plugins: [],
+};
 export default config;
