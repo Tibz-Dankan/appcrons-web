286d75c60474ac784a0121e8388d25f64a2e99c8
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-05-28 12:26:46 +0300

    feat: dyanamic routing functionality to access app's page

--- src/app/app/appList.tsx
+++ src/app/app/appList.tsx
@@ -5,6 +5,8 @@ import { TApp } from "@/types/app";
 import { elapsedTime } from "@/utils/elapsedTime";
 import { truncateString } from "@/utils/truncateString";
 import { ToggleSwitch } from "@/app/shared/toggleSwitch";
+import { clientURL } from "@/constants";
+import { useRouter } from "next/navigation";
 
 interface AppListProps {
   showListHead?: boolean;
@@ -14,9 +16,14 @@ interface AppListProps {
 export const AppList: React.FC<AppListProps> = (props) => {
   const showListHead = props.showListHead ? props.showListHead : true;
   const apps = props.apps;
+  const router = useRouter();
 
   const isLastElement = (list: any[], index: number): boolean => {
     return index === list.length - 1;
+  };
+
+  const navigateToAppPage = (appId: string) => {
+    router.push(`${clientURL}/app/${appId}`);
   };
 
   return (
@@ -57,13 +64,23 @@ export const AppList: React.FC<AppListProps> = (props) => {
                 key={index}
               >
                 <td
-                  className={`px-2 pl-4 border-l-[1px] border-color-border-primary
+                  className={`px-2 pl-4 border-l-[1px] 
+                  border-color-border-primary cursor-pointer
                   ${isLastElement(apps, index) && "rounded-bl-md"}`}
+                  onClick={() => navigateToAppPage(app.id)}
                 >
                   {app.name}
                 </td>
-                <td className="px-2">{truncateString(app.url, 50)}</td>
-                <td className="px-2">
+                <td
+                  className="px-2 cursor-pointer"
+                  onClick={() => navigateToAppPage(app.id)}
+                >
+                  {truncateString(app.url, 50)}
+                </td>
+                <td
+                  className="px-2 cursor-pointer"
+                  onClick={() => navigateToAppPage(app.id)}
+                >
                   {elapsedTime(app.requests[0].startedAt)}
                 </td>
                 <td
