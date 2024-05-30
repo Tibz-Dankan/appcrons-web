8bb4e4ef4ca66cdb14371ade1034aa0521715bfd
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-05-30 20:43:19 +0300

    feat: request pagination under implementation

--- src/app/request/requestList.tsx
+++ src/app/request/requestList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getAccessToken } from "@/utils/getAccessToken";
 import { useAppDispatch } from "@/hooks/redux";
@@ -10,6 +10,8 @@ import { RequestService } from "@/services/request.service";
 import { TRequest } from "@/types/app";
 import { Spinner } from "../shared/loader/spinner";
 import { AppDate } from "@/utils/date";
+import { useRouter, useSearchParams } from "next/navigation";
+// import Button from "../shared/button";
 
 interface RequestListProps {
   appId: string;
@@ -18,14 +20,25 @@ interface RequestListProps {
 export const RequestList: React.FC<RequestListProps> = (props) => {
   const dispatch = useAppDispatch();
   const accessToken = getAccessToken();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  // const [disableNextHandler, setDisableNextHandler] = useState(false);
+  // const [disablePrevHandler, setDisablePrevHandler] = useState(true);
+
+  const before = searchParams.get("before") ? searchParams.get("before")! : "";
+  console.log("before: ", before);
 
   const appId = props.appId;
   console.log("appId :", appId);
 
   const { isPending, error, data } = useQuery({
-    queryKey: [`app-requests-${appId}`],
+    queryKey: [`app-requests-${appId}-${before}`],
     queryFn: () =>
-      new RequestService().getByApp({ appId: appId, accessToken: accessToken }),
+      new RequestService().getByApp({
+        appId: appId,
+        before: before,
+        accessToken: accessToken,
+      }),
   });
 
   useEffect(() => {
@@ -35,7 +48,7 @@ export const RequestList: React.FC<RequestListProps> = (props) => {
     setTimeout(() => {
       dispatch(hideCardNotification());
     }, 5000);
-  }, [error]);
+  }, [error, before]);
 
   const requests: TRequest[] = data && data.data.requests;
 
@@ -47,8 +60,49 @@ export const RequestList: React.FC<RequestListProps> = (props) => {
     return new AppDate(date).dateTime();
   };
 
+  // const loadNextRequestsHandler = () => {
+  //   const createdAtBefore = requests[requests.length - 1].createdAt;
+  //   const twelveHourMillSec = 12 * 1000 * 60 * 60;
+  //   const requestDiffInMillSec =
+  //     new Date().getTime() - new Date(createdAtBefore).getTime();
+
+  //   const disabled: boolean = requestDiffInMillSec >= twelveHourMillSec;
+
+  //   if (disabled) {
+  //     setDisableNextHandler(() => true);
+  //     console.log("next handler disabled::", disabled);
+  //     return;
+  //   }
+
+  //   const currentParams = new URLSearchParams(searchParams);
+  //   currentParams.set("before", createdAtBefore);
+
+  //   router.push(`?before=${createdAtBefore}`);
+  // };
+
+  // const loadPrevRequestsHandler = () => {
+  //   const createdAtBefore = requests[0].createdAt;
+  //   const fiveMinuteMillSec = 5 * 1000 * 60;
+  //   const requestDiffInMillSec =
+  //     new Date().getTime() - new Date(createdAtBefore).getTime();
+
+  //   // validate next btn
+  //   const disabled: boolean = requestDiffInMillSec <= fiveMinuteMillSec;
+
+  //   if (disabled) {
+  //     setDisablePrevHandler(() => true);
+  //     console.log("prev handler disabled::", disabled);
+  //     return;
+  //   }
+
+  //   const currentParams = new URLSearchParams(searchParams);
+  //   currentParams.set("before", createdAtBefore);
+
+  //   router.push(`?before=${createdAtBefore}`);
+  // };
+
   return (
-    <div className="p-4">
+    <div className="p-4 space-y-8">
       {isPending && (
         <div className="w-full grid place-items-center">
           <Spinner className="w-10 h-10" />
@@ -107,6 +161,22 @@ export const RequestList: React.FC<RequestListProps> = (props) => {
           </tbody>
         </table>
       )}
+      {/* {requests && ()}                          */}
+      {/* // <div className="w-full flex items-center justify-end gap-2">
+        //   <Button
+        //     label={"Previous"}
+        //     type={"button"}
+        //     aria-disabled={disablePrevHandler}
+        //     onClick={() => loadPrevRequestsHandler()}
+        //   />
+        //   <Button
+        //     label={"Next"}
+        //     type={"button"}
+        //     aria-disabled={disableNextHandler}
+        //     onClick={() => loadNextRequestsHandler()}
+        //   />
+        // </div> */}
+      //{" "}
     </div>
   );
 };
--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -2,9 +2,9 @@ import { backendURL } from "@/constants";
 import { TGetRequestsByApp } from "@/types/app";
 
 export class RequestService {
-  getByApp = async ({ appId, accessToken }: TGetRequestsByApp) => {
+  getByApp = async ({ appId, before, accessToken }: TGetRequestsByApp) => {
     const response = await fetch(
-      `${backendURL}/requests/get-by-app?appId=${appId}`,
+      `${backendURL}/requests/get-by-app?appId=${appId}&before=${before}`,
       {
         method: "GET",
         headers: {
