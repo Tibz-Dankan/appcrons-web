8de39129b712bc190e52bd74b89d9ddffc7995cb
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-06-01 15:07:05 +0300

    fix: update the prod client url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,6 +6,6 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   clientURL = "http://localhost:3000";
 } else {
   backendURL = "https://keep-active-backend-v2.onrender.com/api/v1";
-  clientURL = "prod url";
+  clientURL = "https://appcrons.netlify.app";
 }
 export { backendURL, clientURL };
