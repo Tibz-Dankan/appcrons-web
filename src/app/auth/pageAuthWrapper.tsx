f5181388228629fce4eb95927ba88c7667f08b12
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-06-16 14:02:22 +0300

    fix: resolve error React Hook 'useAppSelector' cannot be called inside a callback

--- src/app/auth/pageAuthWrapper.tsx
+++ src/app/auth/pageAuthWrapper.tsx
@@ -1,10 +1,14 @@
 "use client";
+
+import React from "react";
 import { useAppSelector } from "@/hooks/redux";
 
 export const PageAuthWrapper = (PageComponent: React.FC) => {
-  const isLoggedIn = useAppSelector((state) => !!state.auth.accessToken);
+  const AuthComponent: React.FC = (props) => {
+    const isLoggedIn = useAppSelector((state) => !!state.auth.accessToken);
 
-  return (props: any) => {
     return <div>{isLoggedIn && <PageComponent {...props} />}</div>;
   };
+
+  return AuthComponent;
 };
