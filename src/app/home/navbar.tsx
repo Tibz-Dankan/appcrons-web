a645a381c66b2db02f4cee71d7f6e410680a0d66
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-06-25 17:39:14 +0300

    feat: add hero section content

---
+++ src/app/home/navbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { ThemeController } from "@/app/shared/themeController";
+import Link from "next/link";
+import { DocumentIcon } from "@/app/shared/Icons/documentIcon";
+
+export const NavBar: React.FC = () => {
+  const pathname = usePathname();
+
+  const isDocsPath = pathname.startsWith("/docs");
+
+  return (
+    <nav
+      className="w-full flex items-center justify-between 
+      p-4 border-b-[1px] border-color-border-primary
+      bg-color-bg-primary fixed top-0 left-0 z-[100]"
+    >
+      <div className="flex items-center gap-2">
+        <Image src="/logo.png" width={40} height={40} alt="logo" />
+        <span
+          className="text-color-text-primary 
+          font-semibold"
+        >
+          AppCrons
+        </span>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <Link
+          href="/docs"
+          className={`flex items-center justify-center gap-2
+           rounded-md p-1 px-3 ${isDocsPath && "bg-header-tab-bg"}
+           hover:text-primary focus:text-primary`}
+        >
+          <DocumentIcon
+            className="text-header-tab-text hover:text-inherit
+            focus:text-inherit"
+          />
+          <span className="hover:text-inherit focus:text-inherit">Docs</span>
+        </Link>
+
+        <ThemeController />
+        <Link
+          href="/auth/login"
+          className={`py-1 px-3 border-[1px] border-color-border-primary
+           rounded-md hover:text-primary focus:text-primary`}
+        >
+          <span>Log In</span>
+        </Link>
+        <Link
+          href="/auth/signup"
+          className={`py-1 px-3 border-[1px]s border-color-border-primary
+           rounded-md bg-primary text-gray-50 hover:bg-primary-light
+           focus:text-primary-light`}
+        >
+          <span>Sign Up</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
-import Image from "next/image";
+import { Hero } from "@/app/home/hero";
+import Button from "@/app/shared/button";
+import Link from "next/link";
+import { NavBar } from "@/app/home/navbar";
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="overflow-x-hidden">
+      <Hero>
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <NavBar />
+          <div
+            className="flex items-center justify-center flex-col
+            max-w-[768px] gap-5 z-10 mt-[10%]"
+          >
+            <h1 className="w-full text-5xl font-bold text-center text-color-text-primary">
+              Optimize uptime for your free backend instance on Render.
+            </h1>
+            <p className="text-center text-lg w-4/5">
+              Appcrons sends automated requests to prevent annoying cold starts
+              and provides the capability to keep within free usage limits.
+            </p>
+            <Link href="/auth/signup" className="">
+              <Button
+                label={"Get started"}
+                type={"button"}
+                className="w-32 bg-primary text-white shadow-md"
+              />
+            </Link>
+          </div>
+        </div>
+      </Hero>
+    </div>
+  );
 }
