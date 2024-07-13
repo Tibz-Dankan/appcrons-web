4fe1e52d722894954224ad087842e318a2d9af1b
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-07-13 18:33:54 +0300

    refactor: change some icons to svg

---
+++ src/app/shared/Icons/deleteIcon.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface DeleteIconProps {
+  className?: string;
+}
+
+export const DeleteIcon: React.FC<DeleteIconProps> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20px"
+      height="20px"
+      viewBox="0 0 24 24"
+      className={twMerge(`text-color-text-primary`, props.className)}
+    >
+      <path
+        fill="currentColor"
+        d="M7 4V2h10v2h5v2h-2v15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V6H2V4zM6 6v14h12V6zm3 3h2v8H9zm4 0h2v8h-2z"
+      />
+    </svg>
+  );
+};
