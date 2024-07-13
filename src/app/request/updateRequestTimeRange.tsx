64d26a4b6d4d557d4a2fead63d9ad49043961591
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-07-13 16:11:46 +0300

    fix: prevent rendering updateRequestTime and deleteRequestTime on the initial page load

--- src/app/request/updateRequestTimeRange.tsx
+++ src/app/request/updateRequestTimeRange.tsx
@@ -29,8 +29,7 @@ import { updateOneApp } from "@/store/actions/app";
 interface UpdateRequestTimeRangeProps {
   requestTimeId: string;
   app: TApp;
-  openModalElement: ReactNode;
-  onUpdate: () => void;
+  onSuccess: (succeeded: boolean) => void;
 }
 
 export const UpdateRequestTimeRange: React.FC<UpdateRequestTimeRangeProps> = (
@@ -38,7 +37,9 @@ export const UpdateRequestTimeRange: React.FC<UpdateRequestTimeRangeProps> = (
 ) => {
   const dispatch = useAppDispatch();
   const accessToken = useAppSelector((state) => state.auth).accessToken;
-  const app = props.app;
+  const app = useAppSelector((state) =>
+    state.app.apps.find((app) => app.id === props.app.id)
+  )!;
   const hasTimeZone: boolean =
     app.requestTimes !== null ? !!app.requestTimes[0]?.timeZone : false;
 
@@ -85,6 +86,10 @@ export const UpdateRequestTimeRange: React.FC<UpdateRequestTimeRangeProps> = (
     dispatch(updateOneApp({ app: currentApp }));
   };
 
+  const onSuccessHandler = (succeeded: true) => {
+    props.onSuccess(succeeded);
+  };
+
   const { isLoading, mutate } = useMutation({
     mutationFn: new RequestService().updateRequestTimeRange,
     onSuccess: async (response: any) => {
@@ -95,6 +100,7 @@ export const UpdateRequestTimeRange: React.FC<UpdateRequestTimeRangeProps> = (
       setTimeout(() => {
         dispatch(hideCardNotification());
       }, 5000);
+      onSuccessHandler(true);
     },
     onError: (error: any) => {
       dispatch(showCardNotification({ type: "error", message: error.message }));
@@ -166,161 +172,146 @@ export const UpdateRequestTimeRange: React.FC<UpdateRequestTimeRangeProps> = (
   const isDisabledSubmitBtn = isLoading || showTimeRangeErrorMessage;
 
   return (
-    <Modal openModalElement={props.openModalElement}>
-      <div
-        className="flex flex-col gap-4 items-center w-[90%] sm:w-96
-        border-[1px]s  border-color-border-primary p-8
-        bg-color-bg-primary rounded-md max-h-[70vh] overflow-x-auto
-        shadow-2xl"
-      >
-        <div className="flex items-center justify-between w-full">
-          <p className="font-semibold">{truncateString(app.name, 40)}</p>
-        </div>
-        <div className="flex items-center justify-start gap-4 w-full">
-          <p className="text-color-text-secondary text-sm">Edit Time Range</p>
-          <p
-            className="flex items-center justify-between gap-1
-            bg-color-bg-secondary p-2 rounded"
-          >
-            <span>{convertTo12HourFormat(getCurrentRequestTime().start)}</span>
-            <span>-</span>
-            <span>{convertTo12HourFormat(getCurrentRequestTime().end)}</span>
-          </p>
-        </div>
-        <div className="w-full space-y-1">
-          <span className="text-sm text-color-text-secondary">
-            Existing Time Ranges
-          </span>
-          <div
-            className="grid grid-cols-2 gap-2 p-4s py-2
-             border-[1px]s border-color-border-primary rounded-md"
-          >
-            {/* TODO: organize time ranges based on the start time in ascending order */}
-            {requestTimeList.map((requestTime, index) => (
-              <p
-                key={index}
-                className={`flex items-center justify-center gap-1 
+    <div
+      className="flex flex-col gap-4 items-center w-[90%] sm:w-96
+      border-[1px]s  border-color-border-primary p-8
+      bg-color-bg-primary rounded-md max-h-[70vh] overflow-x-auto
+      shadow-2xl"
+    >
+      <div className="flex items-center justify-between w-full">
+        <p className="font-semibold">{truncateString(app.name, 40)}</p>
+      </div>
+      <div className="flex items-center justify-start gap-4 w-full">
+        <p className="text-color-text-secondary text-sm">Edit Time Range</p>
+        <p
+          className="flex items-center justify-between gap-1
+          bg-color-bg-secondary p-2 rounded"
+        >
+          <span>{convertTo12HourFormat(getCurrentRequestTime().start)}</span>
+          <span>-</span>
+          <span>{convertTo12HourFormat(getCurrentRequestTime().end)}</span>
+        </p>
+      </div>
+      <div className="w-full space-y-1">
+        <span className="text-sm text-color-text-secondary">
+          Existing Time Ranges
+        </span>
+        <div
+          className="grid grid-cols-2 gap-2 p-4s py-2
+          border-[1px]s border-color-border-primary rounded-md"
+        >
+          {/* TODO: organize time ranges based on the start time in ascending order */}
+          {requestTimeList.map((requestTime, index) => (
+            <p
+              key={index}
+              className={`flex items-center justify-center gap-1 
                  p-2 rounded-md text-[12px]
                  ${
                    getCurrentRequestTime().id === requestTime.id
                      ? "border-[2px] border-primary"
                      : "border-[1px] border-color-border-primary"
                  }`}
-              >
-                <span>{convertTo12HourFormat(requestTime?.start)}</span>
-                <span>-</span>
-                <span>{convertTo12HourFormat(requestTime?.end)}</span>
+            >
+              <span>{convertTo12HourFormat(requestTime?.start)}</span>
+              <span>-</span>
+              <span>{convertTo12HourFormat(requestTime?.end)}</span>
+            </p>
+          ))}
+        </div>
+      </div>
+      <form
+        onSubmit={formik.handleSubmit}
+        className="flex flex-col gap-0 items-center w-[90%] sm:w-full
+        rounded-md z-[1]"
+      >
+        <div className="w-full mb-2">
+          <p
+            className="text-[12px] text-color-text-primary
+            p-2 border-[1px] border-color-border-primary rounded-md"
+          >
+            Edit Time Range by selecting your preferred Start and End time below
+          </p>
+        </div>
+        <div className="w-full">
+          {showTimeRangeSuccessMessage && (
+            <div
+              className="border-[1px] border-color-border-primary rounded-md
+              p-2 w-full mb-2 flex justify-start items-start gap-2"
+            >
+              <span>
+                <IconContext.Provider
+                  value={{
+                    size: "1.2rem",
+                    color: "#55C57A",
+                  }}
+                >
+                  <IoMdCheckmarkCircleOutline />
+                </IconContext.Provider>
+              </span>
+              <p className="text-[#55C57A] text-[14px] text-start">
+                {validateReqTimeRange.message}
               </p>
-            ))}
+            </div>
+          )}
+          {showTimeRangeErrorMessage && (
+            <div
+              className="border-[1px] border-color-border-primary rounded-md
+              p-2 w-full mb-2 flex justify-start items-start gap-2"
+            >
+              <span>
+                <IconContext.Provider
+                  value={{
+                    size: "1.2rem",
+                    color: "#D9534F",
+                  }}
+                >
+                  <MdErrorOutline />
+                </IconContext.Provider>
+              </span>
+              <p className="text-[#D9534F] text-[14px] text-start">
+                {validateReqTimeRange.message}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="w-full flex items-center gap-4 justify-between">
+          <div className="w-full -space-y-5">
+            <label
+              htmlFor="Start"
+              className="text-sm text-color-text-secondary"
+            >
+              Start time
+            </label>
+            <InputSelect label="start" options={timeOptions} formik={formik} />
+          </div>
+          <div className="w-full -space-y-5">
+            <label htmlFor="End" className="text-sm text-color-text-secondary">
+              End time
+            </label>
+            <InputSelect label="end" options={timeOptions} formik={formik} />
           </div>
         </div>
-        <form
-          onSubmit={formik.handleSubmit}
-          className="flex flex-col gap-0 items-center w-[90%] sm:w-full
-           rounded-md z-[1]"
-        >
-          <div className="w-full mb-2">
-            <p
-              className="text-[12px] text-color-text-primary
-               p-2 border-[1px] border-color-border-primary rounded-md"
-            >
-              Edit Time Range by selecting your preferred Start and End time
-              below
-            </p>
-          </div>
-          <div className="w-full">
-            {showTimeRangeSuccessMessage && (
-              <div
-                className="border-[1px] border-color-border-primary rounded-md
-                 p-2 w-full mb-2 flex justify-start items-start gap-2"
-              >
-                <span>
-                  <IconContext.Provider
-                    value={{
-                      size: "1.2rem",
-                      color: "#55C57A",
-                    }}
-                  >
-                    <IoMdCheckmarkCircleOutline />
-                  </IconContext.Provider>
-                </span>
-                <p className="text-[#55C57A] text-[14px] text-start">
-                  {validateReqTimeRange.message}
-                </p>
-              </div>
-            )}
-            {showTimeRangeErrorMessage && (
-              <div
-                className="border-[1px] border-color-border-primary rounded-md
-                 p-2 w-full mb-2 flex justify-start items-start gap-2"
-              >
-                <span>
-                  <IconContext.Provider
-                    value={{
-                      size: "1.2rem",
-                      color: "#D9534F",
-                    }}
-                  >
-                    <MdErrorOutline />
-                  </IconContext.Provider>
-                </span>
-                <p className="text-[#D9534F] text-[14px] text-start">
-                  {validateReqTimeRange.message}
-                </p>
-              </div>
-            )}
-          </div>
-          <div className="w-full flex items-center gap-4 justify-between">
-            <div className="w-full -space-y-5">
-              <label
-                htmlFor="Start"
-                className="text-sm text-color-text-secondary"
-              >
-                Start time
-              </label>
-              <InputSelect
-                label="start"
-                options={timeOptions}
-                formik={formik}
-              />
-            </div>
-            <div className="w-full -space-y-5">
-              <label
-                htmlFor="End"
-                className="text-sm text-color-text-secondary"
-              >
-                End time
-              </label>
-              <InputSelect label="end" options={timeOptions} formik={formik} />
-            </div>
-          </div>
 
-          <Button
-            label={
-              <>
-                {!isLoading && (
-                  <span
-                    className={`${
-                      showTimeRangeErrorMessage && "text-[#adb5bd]"
-                    }`}
-                  >
-                    Submit
-                  </span>
-                )}
-                {isLoading && (
-                  <Spinner
-                    label="processing"
-                    className="w-5 h-5 text-gray-100"
-                  />
-                )}
-              </>
-            }
-            type="submit"
-            disabled={isDisabledSubmitBtn}
-            className="w-full mt-6 font-semibold"
-          />
-        </form>
-      </div>
-    </Modal>
+        <Button
+          label={
+            <>
+              {!isLoading && (
+                <span
+                  className={`${showTimeRangeErrorMessage && "text-[#adb5bd]"}`}
+                >
+                  Submit
+                </span>
+              )}
+              {isLoading && (
+                <Spinner label="processing" className="w-5 h-5 text-gray-100" />
+              )}
+            </>
+          }
+          type="submit"
+          disabled={isDisabledSubmitBtn}
+          className="w-full mt-6 font-semibold"
+        />
+      </form>
+    </div>
   );
 };
