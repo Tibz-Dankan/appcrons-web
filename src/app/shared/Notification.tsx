5f0a61359527d67af94cadacacc2c8d6ace4a784
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-08-09 01:09:49 +0300

    feat: use react portal for rendering notification element

--- src/app/shared/Notification.tsx
+++ src/app/shared/Notification.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client";
+
+import ReactDOM from "react-dom";
+import React, { Fragment } from "react";
 import { CheckIcon } from "@/app/shared/Icons/CheckIcon";
 import { ErrorIcon } from "@/app/shared/Icons/ErrorIcon";
 import { InfoIcon } from "@/app/shared/Icons/InfoIcon";
 import { WarningIcon } from "@/app/shared/Icons/WarningIcon";
 import { CloseIcon } from "@/app/shared/Icons/CloseIcon";
+import { useIsClient } from "@/hooks/UseIsClient";
 
 interface NotificationProps {
   type: string | null;
@@ -52,32 +56,53 @@ export const Notification: React.FC<NotificationProps> = (props) => {
     borderColor = "border-info";
   }
 
+  const isClient = useIsClient();
+
+  const createAppendPortalElement = () => {
+    if (!isClient) return;
+    const portalElement = document.createElement("div");
+    portalElement.setAttribute("id", "notification");
+    const body = document.body;
+    body.appendChild(portalElement);
+  };
+  createAppendPortalElement();
+
   return (
-    <div className="w-full fixed top-0 grid place-items-center">
-      <div
-        className={`border-[1px] border-color-border-primary 
-         absolute top-5 z-[10000] flex w-72 animate-slideDown
-         flex-col items-start rounded-[2px] 
-         bg-color-bg-secondary py-2 pl-6 text-lg shadow-2xl`}
-      >
-        <span
-          className="absolute right-3 top-3 cursor-pointer"
-          onClick={props.onClose}
-        >
-          <CloseIcon className="text-[#868e96] w-6 h-6" />
-        </span>
+    <Fragment>
+      {ReactDOM.createPortal(
         <div
-          className={`absolute left-0 top-0 h-full w-3 ${bgColor}
-           border-y-[1px] rounded-l-[2px] ${borderColor}`}
-        />
-        <div className={`flex items-center justify-start gap-3 ${textColor}`}>
-          <div>{icon}</div>
-          <span className="first-letter:uppercase">{title}</span>
-        </div>
-        <div className="text-color-text-primary">
-          <span className="text-sm leading-[4px]">{props.message}</span>
-        </div>
-      </div>
-    </div>
+          className="w-full fixed top-0 grid place-items-center
+           z-[80]"
+        >
+          <div
+            className={`border-[1px] border-color-border-primary 
+             absolute top-5 z-[10000] flex w-72 animate-slideDown
+             flex-col items-start rounded-[2px] 
+             bg-color-bg-secondary py-2 pl-6 text-lg shadow-2xl`}
+          >
+            <span
+              className="absolute right-3 top-3 cursor-pointer"
+              onClick={props.onClose}
+            >
+              <CloseIcon className="text-[#868e96] w-6 h-6" />
+            </span>
+            <div
+              className={`absolute left-0 top-0 h-full w-3 ${bgColor}
+               border-y-[1px] rounded-l-[2px] ${borderColor}`}
+            />
+            <div
+              className={`flex items-center justify-start gap-3 ${textColor}`}
+            >
+              <div>{icon}</div>
+              <span className="first-letter:uppercase">{title}</span>
+            </div>
+            <div className="text-color-text-primary">
+              <span className="text-sm leading-[4px]">{props.message}</span>
+            </div>
+          </div>
+        </div>,
+        document.getElementById("notification")!
+      )}
+    </Fragment>
   );
 };
