bfe5a0f79935c93c730e0c8a025ef31b7fe4ed6b
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-08-23 13:27:17 +0300

    refactor: update dev port number of the server

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@
 let backendURL: string;
 let clientURL: string;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  backendURL = "http://localhost:8000/api/v1";
+  backendURL = "http://localhost:8080/api/v1";
   clientURL = "http://localhost:3000";
 } else {
   backendURL = "https://keep-active-backend-v2.onrender.com/api/v1";
