cd27e2d1f7a11306a9fb638d56aab2a14e4b3a20
Tibz-Dankan <tibesigwadankan@gmail.com>
2026-04-17 19:19:15 +0300

    refactor: update urls to correct values depending on the environment they are running in

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,16 +5,11 @@ let clientURL: string;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   backendURL = "http://localhost:8080/api/v1";
   clientURL = "http://localhost:3000";
-  console.log("process.env.NODE_ENV :", process.env.NODE_ENV);
 } else if (process.env.NODE_ENV === "test") {
-  // backendURL = "https://keep-active-backend-v2.onrender.com/api/v1";
   backendURL = "http://localhost:8080/api/v1";
   clientURL = "http://localhost:3000";
-  console.log("process.env.NODE_ENV :", process.env.NODE_ENV);
 } else {
-  // backendURL = "https://keep-active-backend-v2.onrender.com/api/v1";
-  backendURL = "http://localhost:8080/api/v1";
-  clientURL = "http://localhost:3000";
-  console.log("process.env.NODE_ENV :", process.env.NODE_ENV);
+  backendURL = "https://keep-active-backend-v2.onrender.com/api/v1";
+  clientURL = "https://appcrons.netlify.app";
 }
 export { backendURL, clientURL };
